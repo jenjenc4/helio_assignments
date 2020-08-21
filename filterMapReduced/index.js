87777b2c37c7d67b00434c57d311bd9b413bf844
jenjenc4 <jenjen@heliotraining.com>
2020-08-21 11:29:32 -0600

    initial commit

---
+++ filterMapReduced/index.js
@@ -0,0 +1,70 @@
+import Senators from './data/senators'
+
+export const republicans = () => {
+return Senators.filter(element =>element.party === "Republican")
+  
+}
+export const democrats = () => {
+  return Senators.filter(element =>element.party === "Democrat")
+}
+
+export const independents = () => {
+  return Senators.filter(element =>element.party === "Independent")
+}
+
+export const males = () => {
+  return Senators.filter(element =>element.person.gender === "male")
+}
+
+export const females = () => {
+  return Senators.filter(element =>element.person.gender === "female")
+}
+
+export const byState = (state = 'UT') => {
+  return Senators.filter(element =>element.state === "UT")
+}
+
+export const mapping = () => {
+  return Senators.map(element => {
+    return { 
+      firstName: element.person.firstname,
+      lastName: element.person.lastname,
+      party: element.party,
+      gender: element.person.gender,
+    }
+  })
+}
+
+export const reducedCount = () => {
+  return Senators.reduce((accumulator, element) => {
+
+   if (element.party === "Democrat")
+   {
+     accumulator.democrat += 1
+   }
+   
+   if (element.party === "Republican")
+   {
+     accumulator.republican += 1
+   }
+
+   if (element.party === "Independent")
+   {
+     accumulator.independent += 1
+   }
+    
+   return accumulator
+
+  },
+
+  {
+    republican: 0,
+    democrat: 0,
+    independent: 0  
+  })
+  
+  }
+
+
+
+const REPLACE_ME_WITH_CODE = false
